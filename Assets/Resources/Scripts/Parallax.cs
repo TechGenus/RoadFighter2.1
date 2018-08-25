2de1a9ce5b320a9028b72fed070afcc9bd161077
Calvin0630 <cg7057705@gmail.com>
2018-08-25 16:39:07 -0400

    parallax working

--- Assets/Resources/Scripts/Parallax.cs
+++ Assets/Resources/Scripts/Parallax.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour {
 	public float scrollSpeed;
+	public Vector2 scrollDir;
 	GameObject rectPrefab;
 	bool first = true;
 	Canvas canvas;
+	//screen size in pixels
+	Vector2 screenSize;
 	// Use this for initialization
 	void Start () {
 		rectPrefab = gameObject;
 		canvas = transform.parent.GetComponent<Canvas>();
-		Debug.Log(canvas.enabled);
+		Screen.SetResolution(1920,1080,true);
+		screenSize = new Vector2(GetComponentInParent<RectTransform>().rect.width, GetComponentInParent<RectTransform>().rect.height);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 localPos = GetComponent<RectTransform>().localPosition;
+		//scroll dir: (x,y) st Max(x,y) == 1
+		float max = Mathf.Max(Mathf.Abs(scrollDir.x), Mathf.Abs(scrollDir.y));
+		scrollDir = scrollDir/max;
+		Debug.Log(scrollDir);
+		//Get the position of the object relative to the parent in pixel soace (0,0) is the middle of screen
+		Vector2 localPos = new Vector2(GetComponent<RectTransform>().localPosition.x, GetComponent<RectTransform>().localPosition.y);
+
 
 		//if the image is past the middle
-		if (first&&localPos.x>0) {
+		if (first&&Vector2.Dot(localPos,scrollDir)>=0) {
 			//the location of the newly instantiated image
-			Vector2 pos = new Vector2(-(GetComponent<RectTransform>().rect.width), 0);
+			Vector2 newPos = -(new Vector2(screenSize.x*scrollDir.x, screenSize.y*scrollDir.y));
+			Debug.Log(newPos);
 			//instantiate a new image
 			GameObject next = (GameObject) Instantiate(rectPrefab,transform.parent);
-			next.GetComponent<RectTransform>().localPosition = pos;
+			next.GetComponent<RectTransform>().localPosition = newPos;
 			next.transform.SetParent(this.transform.parent);
 			next.name = gameObject.name;
 			//next.GetComponent<RectTransform>().sizeDelta = GetComponent<RectTransform>().sizeDelta;
@@ -32,10 +43,12 @@ public class Parallax : MonoBehaviour {
 
 		}
 		//if its off the screen
-		else if (!first&&localPos.x>GetComponent<RectTransform>().rect.width ) {
+		else if (!first&&Mathf.Abs(localPos.x)>=Mathf.Abs(screenSize.x*scrollDir.x)&&Mathf.Abs(localPos.y)>=Mathf.Abs(screenSize.y*scrollDir.y) ) {
 			Destroy(gameObject);
 		}
-		transform.Translate(Vector3.right*scrollSpeed);
+		//The relative movement of the object
+		Vector2 relativeDir = new Vector2(scrollDir.x*screenSize.x, scrollDir.y*screenSize.y).normalized;
+		transform.Translate(relativeDir);
 
 	}
 }
