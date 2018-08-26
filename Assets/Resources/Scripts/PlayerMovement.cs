0724d25e4b5670b1bfc3acc0943d0bd7b08b113d
Janak <Janakkumar.Bhanushal@ryerson.ca>
2018-08-25 22:46:38 -0400

    Added movement and animation to playerTwo

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -2,79 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
 	public Vector2 speed;
-	public float knockBackTime = 2f;
+	public float knockBackTime = 1f;
+	public GameObject otherPlayer;
+	public string horizontalAxisName;
+	public string verticalAxisName;
+	public string playerAnimationStateName;
 
-	private Transform t;
+	private Vector2 deltaSpeed;
 	private Animator anim;
 	private Camera cam;
-
 	private float controlFreezeTime;
 	private float maxY;
+	private Transform player1T;
+	private float hAxis;
+	private float vAxis;
 
 	// Use this for initialization
-	void Start()
-	{
+	void Start() {
 		cam = Camera.main;
 		maxY = cam.orthographicSize;
 		Debug.Log("maxY = " + maxY);
 
-		t = GetComponent<Transform>();
+		player1T = GetComponent<Transform>();
 		anim = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
-	void Update()
-	{
-		if (anim.GetInteger("playerOneState") == 2 && Time.time <= controlFreezeTime) {
-			t.Translate(new Vector3(-speed.x, -speed.y, 0) * Time.deltaTime);
+	void Update() {
+		deltaSpeed = speed * Time.deltaTime;
+		hAxis = Input.GetAxisRaw(horizontalAxisName) * deltaSpeed.x;
+		vAxis = Input.GetAxis(verticalAxisName) * deltaSpeed.y;
+
+		if (Time.time <= controlFreezeTime) {
+			if (anim.GetInteger(playerAnimationStateName) == 2) {
+				player1T.Translate(new Vector3(-deltaSpeed.x, -deltaSpeed.y, 0));
+			} else if (anim.GetInteger(playerAnimationStateName) == 3) {
+				player1T.Translate(new Vector3(deltaSpeed.x, -deltaSpeed.y, 0));
+			}
+		} else {
+			player1T.Translate(new Vector3(hAxis, vAxis, 0));
+
+			if (vAxis != 0 || hAxis != 0) {
+				anim.SetInteger(playerAnimationStateName, 1);
+			} else {
+				anim.SetInteger(playerAnimationStateName, 0);
+			}
 		}
-		else if (anim.GetInteger("playerOneState") == 3 && Time.time <= controlFreezeTime) {
-			t.Translate(new Vector3(speed.x, -speed.y, 0) * Time.deltaTime);
-		}
-		else {
-			DriveNormally();
-		}
+
 		CheckWinOrLoseConditions();
 	}
 
-	void OnCollisionEnter2D(Collision2D col)
-	{
-		Debug.Log(col.transform.position.x - t.position.x);
-		if (col.transform.position.x - t.position.x >= 0) { // slide right
-			anim.SetInteger("playerOneState", 2);
-		}
-		else { // slide left
-			anim.SetInteger("playerOneState", 3);
-		}
-		controlFreezeTime = Time.time + knockBackTime;
-		Debug.Log(controlFreezeTime);
-	}
-
-	void DriveNormally()
-	{
-		float hAxis = Input.GetAxis("Horizontal") * speed.x;
-		float vAxis = Input.GetAxis("Vertical") * speed.y;
-		Vector3 translationDistance = new Vector3(hAxis, vAxis, 0) * Time.deltaTime;
-		t.Translate(translationDistance);
-
-		if (vAxis > 0 || hAxis != 0) {
-			anim.SetInteger("playerOneState", 1);
-		}
-		else {
-			anim.SetInteger("playerOneState", 0);
-		}
-	}
-
-	void CheckWinOrLoseConditions()
-	{
-		if (t.position.y > maxY) {
+	void CheckWinOrLoseConditions() {
+		if (player1T.position.y > maxY) {
 			Debug.Log("You win");
-		}
-		else if (t.position.y < -maxY) {
+		} else if (player1T.position.y < -maxY) {
 			Debug.Log("You lose");
 		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.transform.position.x - player1T.position.x >= 0) { // slide right
+			anim.SetInteger(playerAnimationStateName, 2);
+		} else { // slide left
+			anim.SetInteger(playerAnimationStateName, 3);
+		}
+		controlFreezeTime = Time.time + knockBackTime;
 	}
 }
