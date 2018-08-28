d513bab827a413e45c487b6c38b482fca39e99a7
Calvin0630 <cg7057705@gmail.com>
2018-08-28 15:00:11 -0400

    updated build settings and workiing on UI

---
+++ Assets/Resources/Scripts/MenuNavigation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour {
+
+	public void StartGame() {
+		Debug.Log("start game");
+	}
+	public void ShowControls() {
+		Debug.Log("Show Controls");
+	}
+	public void LeaveGame() {
+		Debug.Log("leave game");
+	}
+}
