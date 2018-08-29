ea13889edc6a3c804b9e51346a8d2e2f2477d56b
Calvin0630 <cg7057705@gmail.com>
2018-08-29 19:02:03 -0400

    menu working, working on port

--- Assets/Resources/Scripts/MenuNavigation.cs
+++ Assets/Resources/Scripts/MenuNavigation.cs
@@ -16,4 +16,19 @@ public class MenuNavigation : MonoBehaviour {
 		Debug.Log("leave game");
 		Application.Quit();
 	}
+
+	void Start() {
+
+	}
+	void Update() {
+		float joystickY = Input.GetAxis("Player1_Vertical");
+		float joystickX = Input.GetAxis("Player1_Horizontal");
+		//Debug.Log("Player1: ("+joystickX+", "+joystickY+")");
+		joystickY = Input.GetAxis("Player2_Vertical");
+		joystickX = Input.GetAxis("Player2_Horizontal");
+		//Debug.Log("Player2: ("+joystickX+", "+joystickY+")");
+		if (Input.GetButtonDown("A")) {
+			Debug.Log("A");
+		}
+	}
 }
